717e52e472d763b5ddeef7ed45da711239f1863b
aceon2ndserve <stelios.zoit@gmail.com>
2023-02-24 14:39:51 +0200

    Revert "Body implementation"
    This reverts commit 7a5ce7614d6d793bf176acc15bf12152b2af2584.

--- src/routers/router.ts
+++ src/routers/router.ts
@@ -1,48 +1,16 @@
 import { Router } from "express";
 import axios from "axios";
-require("dotenv").config();
 
 const router = Router();
-const username = process.env.USERNAME;
-const password = process.env.PASSWORD;
 
-const API_TEST = `https://lt.integration.dpd.eo.pl/ws-mapper-rest/createShipment_?username=${username}&password=${password}&`;
+const API_TEST = `https://lt.integration.dpd.eo.pl/ws-mapper-rest/createShipment_?username=testuser1&password=testpassword1&`;
 
 export default router.post("/", async (req, res) => {
-  try {
-    const dataReceived = req.body;
-    const {
-      name,
-      address,
-      city,
-      country,
-      zip,
-      num_of_parcel = 1,
-      parcel_type = "D",
-      phone,
-    } = dataReceived.address_to;
-    const { width, height, length } = dataReceived.items[0];
-    if (
-      !name ||
-      !address ||
-      !city ||
-      !country ||
-      !zip ||
-      !num_of_parcel ||
-      !parcel_type ||
-      !phone ||
-      !width ||
-      !height ||
-      !length
-    ) {
-      res.json("Please provide all the information");
-      return;
-    }
-    const { data } = await axios.post(
-      `${API_TEST}name1=${name}&street=${address}&country=${country}&city=${city}&pcode=${zip}&phone=${phone}&num_of_parcel=${num_of_parcel}&parcel_type=${parcel_type}&width=${width}&height=${height}&length=${length}`
-    );
-    res.json(data);
-  } catch (error) {
-    res.json(error);
-  }
+  const dataGiven = Object.keys(req.body)
+    .map(function (k) {
+      return encodeURIComponent(k) + "=" + encodeURIComponent(req.body[k]);
+    })
+    .join("&");
+  const { data } = await axios.post(`${API_TEST}${dataGiven}`);
+  res.json(data);
 });
